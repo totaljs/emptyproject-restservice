3435e7b624977fcac8d5a4da220b711b6e9d8fd6
Peter Širka <petersirka@gmail.com>
2017-09-06 08:35:52 +0200

    Update start scripts.

--- test.js
+++ test.js
@@ -1,22 +1,14 @@
 // ===================================================
-// IMPORTANT: only for testing
-// total.js - web application framework for node.js
-// http://www.totaljs.com
+// FOR UNIT-TESTING
+// Total.js - framework for Node.js platform
+// https://www.totaljs.com
 // ===================================================
 
-var fs = require('fs');
-var options = {};
+const options = {};
 
-// options.tests = ['controllers', 'modules'];
 // options.ip = '127.0.0.1';
 // options.port = parseInt(process.argv[2]);
-// options.config = { name: 'total.js' };
-// options.https = { key: fs.readFileSync('keys/agent2-key.pem'), cert: fs.readFileSync('keys/agent2-cert.pem')};
-// options.sleep = 2000;
-
-/**
- * Release notes:
- */
+// options.config = { name: 'Total.js' };
+// options.sleep = 3000;
 
 require('total.js').http('test', options);
-// require('total.js').https('test', options);
