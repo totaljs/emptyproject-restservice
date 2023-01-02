0c4ebf846c19d769ef06e7dddca909529155bf79
Peter Širka <petersirka@gmail.com>
2023-01-02 10:11:17 +0100

    Updated example.

--- controllers/api.js
+++ controllers/api.js
@@ -3,9 +3,9 @@ exports.install = function() {
 	// Sets cors for the entire API
 	CORS();
 
-	ROUTE('GET    /api/users/        *Users --> query');
-	ROUTE('GET    /api/users/{id}/   *Users --> read');
-	ROUTE('POST   /api/users/        *Users --> insert');
-	ROUTE('PUT    /api/users/{id}/   *Users --> update');
-	ROUTE('DELETE /api/users/{id}/   *Users --> remove');
+	ROUTE('GET    /api/users/          *Users --> query');
+	ROUTE('GET    /api/users/{id}/     *Users --> read');
+	ROUTE('POST   /api/users/          *Users --> insert');
+	ROUTE('PUT    /api/users/{id}/     *Users --> update');
+	ROUTE('DELETE /api/users/{id}/     *Users --> remove');
 };
--- schemas/users.js
+++ schemas/users.js
@@ -5,47 +5,59 @@ NEWSCHEMA('Users', function(schema) {
 	schema.define('email', 'Email', true);
 	schema.define('phone', 'Phone');
 
-	schema.setQuery(function($) {
-
-		var builder = NOSQL('users').find();
-		$.query.search && builder.search('search', $.query.search);
-		builder.fields('id,firstname,lastname,dtcreated');
-		builder.sort('dtcreated_desc');
-		builder.callback($.callback);
-
+	schema.action('query', {
+		name: 'Returns list of users',
+		query: 'q:String',
+		action: function($) {
+			var builder = DB().find('nosql/users');
+			$.query.search && builder.search('search', $.query.search);
+			builder.fields('id,firstname,lastname,dtcreated');
+			builder.sort('dtcreated_desc');
+			builder.callback($.callback);
+		}
 	});
 
-	schema.setRead(function($) {
-
-		// Reads the user
-		NOSQL('users').one().error(404).where('id', $.id).callback($.callback);
-
+	schema.action('read', {
+		name: 'Reads a user profile',
+		params: 'id:UID',
+		action: function($) {
+			var params = $.params;
+			DB().one('nosql/users').error('@(User not found)').where('id', params.id).callback($.callback);
+		}
 	});
 
-	schema.setInsert(function($, model) {
+	schema.action('create', {
+		name: 'Creates a new user profile',
+		action: async function($, model) {
 
-		model.id = UID();
-		model.dtcreated = NOW;
-		model.search = (model.firstname + ' ' + model.lastname).toSearch();
+			model.id = UID();
+			model.dtcreated = NOW;
+			model.search = (model.firstname + ' ' + model.lastname).toSearch();
 
-		// Inserts data
-		NOSQL('users').insert(model).callback($.done(model.id));
-
+			// Inserts data
+			await DB().insert('nosql/users', model).promise($);
+			$.success(model.id);
+		}
 	});
 
-	schema.setUpdate(function($, model) {
-
-		model.dtupdated = NOW;
-
-		// Modifies data
-		NOSQL('users').modify(model).where('id', $.id).callback($.done($.id));
-
+	schema.action('update', {
+		name: 'Updates a user profile',
+		params: 'id:UID',
+		action: async function($, model) {
+			var params = $.params;
+			model.dtupdated = NOW;
+			await DB.modify('nosql/users', model).id(params.id).error('@(User not found)').promise($);
+			$.success(params.id);
+		}
 	});
 
-	schema.setRemove(function($) {
-
-		// Removes the user
-		NOSQL('users').remove().where('id', $.id).callback($.done($.id));
-
+	schema.action('Remove', {
+		name: 'Removes a user',
+		params: 'id:UID',
+		action: async function($) {
+			var params = $.params;
+			await DB().remove('nosql/users').id(params.id).error('@(User not found)').promise($);
+			$.success(params.id);
+		}
 	});
 });
